e05ca21d50bdc4189a12a4b68ddda9fe2e8913da
BeainaT <tania.df@hotmail.it>
2022-04-27 16:47:04 +0200

    added css style + hidden box

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ const Name = prompt("Enter your name");
 //ask for user surname and save it
 const surName = prompt("Enter your surname. In case of several surname, please don't leave space between each other");
 //ask for user favorite color
-const color = prompt("What is your favorite color?");
+const color = prompt("Enter your favorite color");
 //print namesurnamefavoritecolor21 on document
 document.getElementById("pwd").innerHTML = (Name + surName + color + 21);
+//print fake password in overlay container
+document.getElementById("hidden_pwd").innerHTML = (Name + surName + color + 21);
+
