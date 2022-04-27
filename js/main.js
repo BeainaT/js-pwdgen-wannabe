086bf79523e2ee58fc5f8b79988d12986362da57
BeainaT <tania.df@hotmail.it>
2022-04-27 15:09:41 +0200

    added some html and js prompt

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 //ask for user name and save it
+const Name = prompt("Enter your name");
 //ask for user surname and save it
+const surName = prompt("Enter your surname. In case of several surname, please don't leave space between each other");
 //ask for user favorite color
-//print namesurnamefavoritecolor21
+const color = prompt("What is your favorite color?");
+//print namesurnamefavoritecolor21 on document
+document.getElementById("pwd").innerHTML = (Name + surName + color + 21);
